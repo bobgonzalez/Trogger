39c6e4c90b47f8bbe7ebfdb15d8aad29e6ca7ec7
zero <labratjazzcat@outlook.com>
2017-01-09 21:43:02 -0800

    proper indentation

--- trogger.cpp
+++ trogger.cpp
@@ -36,24 +36,24 @@ log log4(70, 5, FALSE);
 void level_1 (std::vector< std::vector < int > >& cache, frog& frogger, room& the_room){
   int j, i;
   
-//  the_room.new_exit(COLS/2, 1);
-//  the_room.refresh_exit();
-//  the_room.set_start(COLS/2, LINES-3);
+  //  the_room.new_exit(COLS/2, 1);
+  //  the_room.refresh_exit();
+  //  the_room.set_start(COLS/2, LINES-3);
   if (cycle % 2 == 1){
-  for (j = 0; j < LINES; j++){
-    for (i = 0; i < COLS; i++){
-      if (j == 16 || j == 5){
-        attrset(COLOR_PAIR(2));
-        cache[i][j] = 2;
-//        the_room.cache[i][j] = 2;
-        mvaddch(j, i, ' ');
+    for (j = 0; j < LINES; j++){
+      for (i = 0; i < COLS; i++){
+	if (j == 16 || j == 5){
+	  attrset(COLOR_PAIR(2));
+	  cache[i][j] = 2;
+	  //        the_room.cache[i][j] = 2;
+	  mvaddch(j, i, ' ');
+	}
       }
     }
-  }
-  log4.advance(cache, frogger);
-  log3.advance(cache, frogger);
-  log1.advance(cache, frogger);
-  log2.advance(cache, frogger);
+    log4.advance(cache, frogger);
+    log3.advance(cache, frogger);
+    log1.advance(cache, frogger);
+    log2.advance(cache, frogger);
   }
   if (cycle % 2 == 1 || cycle % 3 == 2)
     train1.advance(cache);
@@ -77,33 +77,33 @@ void level_3(){
 
 void levels(int level, std::vector< std::vector < int > >& cache, frog& frogger, room& the_room){
   switch (level){
-    case 1: 
-      level_1(cache, frogger, the_room);
-      break;
-    case 2: 
-      level_2();
-      break;
-    case 3: 
-      level_3();
-      break;
+  case 1: 
+    level_1(cache, frogger, the_room);
+    break;
+  case 2: 
+    level_2();
+    break;
+  case 3: 
+    level_3();
+    break;
   }
   return;
 }
 bool death (int score, int level){
-    init_pair(6,COLOR_WHITE, COLOR_BLACK);
+  init_pair(6,COLOR_WHITE, COLOR_BLACK);
   attrset(COLOR_PAIR(6));
   attron(A_REVERSE) ;
-        mvprintw(LINES/2, (COLS/2)-20, "YOU DIED!!! at level %d with %d points", level, score);
-        mvprintw(LINES/2-1, (COLS/2)-20, "press 1 to play again and 0 to quit");
-        refresh();
-        char key;
-        do {
-        key = getch();
-        if (key == '1')
-                return false;
-        else if (key == '0')
-                return true;
-        } while (key != '\n');
+  mvprintw(LINES/2, (COLS/2)-20, "YOU DIED!!! at level %d with %d points", level, score);
+  mvprintw(LINES/2-1, (COLS/2)-20, "press 1 to play again and 0 to quit");
+  refresh();
+  char key;
+  do {
+    key = getch();
+    if (key == '1')
+      return false;
+    else if (key == '0')
+      return true;
+  } while (key != '\n');
 }
 void win (){
   return;
@@ -137,7 +137,7 @@ int main (void){
   the_room.set_start(COLS/2, LINES-3);
   if (debug)
     lifes = -1;
-
+  
   initscr();
   keypad(stdscr,TRUE);
   start_color();
@@ -156,7 +156,7 @@ int main (void){
   x_pos = COLS/2;
   y_pos = LINES-3;
   frog frogger = frog(COLS/2, LINES-3);
-
+  
   do{
     levels(level, cache, frogger, the_room);
     cycle++;
@@ -166,53 +166,53 @@ int main (void){
     int x = frogger.get_x();
     int y = frogger.get_y();
     switch(key){
-      case 'j':
-      case KEY_DOWN:
-        moveNum++;
-        move(y, x);
-        attrset(COLOR_PAIR(cache[x][y]));
-        frogger.set_y(y+1);
-        addch(' ');
-        if (frogger.get_y() > LINES-2) 
-          frogger.set_y(LINES-2);
-        break;
-      case 'k':
-      case KEY_UP:
-        moveNum++;
-        move(y, x);
-        attrset(COLOR_PAIR(cache[x][y]));
-        frogger.set_y(y-1);
+    case 'j':
+    case KEY_DOWN:
+      moveNum++;
+      move(y, x);
+      attrset(COLOR_PAIR(cache[x][y]));
+      frogger.set_y(y+1);
       addch(' ');
-        if (frogger.get_y() < 0) 
-          frogger.set_y(0);
-        break;
-      case 'h':
-      case KEY_LEFT:
-        moveNum++;
-        move(y, x);
-        attrset(COLOR_PAIR(cache[x][y]));
-        frogger.set_x(x-1);
-        addch(' ');
-        if (frogger.get_x() < 0) 
-          frogger.set_x(COLS-1);
-        break;
-      case 'l':
-      case KEY_RIGHT:
-        moveNum++;
-        move(y, x);
-        attrset(COLOR_PAIR(cache[x][y]));
-        frogger.set_x(x+1);
-        addch(' ');
-        if (frogger.get_x() > COLS) 
-          frogger.set_x(0);
-        break;
-      default:
-        break;
-       }
-  if (cache[x][y] == 4){
+      if (frogger.get_y() > LINES-2) 
+	frogger.set_y(LINES-2);
+      break;
+    case 'k':
+    case KEY_UP:
+      moveNum++;
+      move(y, x);
+      attrset(COLOR_PAIR(cache[x][y]));
+      frogger.set_y(y-1);
+      addch(' ');
+      if (frogger.get_y() < 0) 
+	frogger.set_y(0);
+      break;
+    case 'h':
+    case KEY_LEFT:
+      moveNum++;
+      move(y, x);
+      attrset(COLOR_PAIR(cache[x][y]));
+      frogger.set_x(x-1);
+      addch(' ');
+      if (frogger.get_x() < 0) 
+	frogger.set_x(COLS-1);
+      break;
+    case 'l':
+    case KEY_RIGHT:
+      moveNum++;
+      move(y, x);
+      attrset(COLOR_PAIR(cache[x][y]));
+      frogger.set_x(x+1);
+      addch(' ');
+      if (frogger.get_x() > COLS) 
+	frogger.set_x(0);
+      break;
+    default:
+      break;
+    }
+    if (cache[x][y] == 4){
       beep();
       flash();
-//      level++;
+      //      level++;
       lifes++;
       frogger.set_x(COLS/2);
       frogger.set_y(LINES-3);
@@ -223,14 +223,14 @@ int main (void){
       if (lifes == 0){
         bool quit = death(score, level);
         if (quit == true){
-            return 0;
-            // refresh map
+	  return 0;
+	  // refresh map
         }
         else {
-        lifes = 0;
-        level = 1; 
-        score = 0;
-        frogger = frog(COLS/2, LINES-3);
+	  lifes = 0;
+	  level = 1; 
+	  score = 0;
+	  frogger = frog(COLS/2, LINES-3);
         }
       }
       else{
@@ -248,7 +248,7 @@ int main (void){
     cache[COLS/2][1]=4;
     attrset(COLOR_PAIR(6));
     attron(A_REVERSE) ;
-    mvprintw(LINES-1, 0, "       Time:  %d        Score:  %d    x:%d, y:%d     Lifes: %d    Level: %d          ", cycle, score, x_pos, y_pos, lifes, level);
+    mvprintw(LINES-1, 0, "       Time:  %d        Score:  %d    x:%d, y:%d     Lifes: %d    Level: %d          ", cycle, score, x, y, lifes, level);
     attroff(A_REVERSE);
     move(y, x);
     addch('+');
